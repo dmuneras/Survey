5e218957283d5304d20117ea8d52ccf068a81742
Daniel Múnera Sánchez <dmunera119@gmail.com>
2010-12-01 21:40:18 -0500

    reverse translation routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,26 @@ ActionController::Routing::Routes.draw do |map|
  # -> Definicion de las rutas del proyecto. POSDATA: todo esta dentro del path_names porque deseamos que las rutas fueran
  #     en español.
 
-  map.with_options :path_names => {:new => 'nuevo', :show => 'mostrar', :edit => 'editar',:create => 'crear'} do |r|
+  # map.with_options :path_names => {:new => 'nuevo', :show => 'mostrar', :edit => 'editar',:create => 'crear'} do |r|
 
-    #definicion de la ruta anidad /empresa/usuarios
-    r.resources :companies,:as=> 'empresas' do |company|
-      company.resources :users, :as => 'usuarios', 
-        :path_names => {:new => 'nuevo', :edit => 'editar', :create => 'crear'}, :shallow => true 
-    end
-    r.resources :surveys,    :as => 'encuestas'
-    r.resources :surveys,    :member => {:next_question => :get}
-    r.resources :answers,    :as => 'respuestas' 
-    r.resources :questions,  :as => 'preguntas'
-    r.resources :subsectors, :as => 'subsectores'
-    r.resources :chart,      :as => 'resultados'
-  end
+  #   #definicion de la ruta anidad /empresa/usuarios
+  #   r.resources :companies,:as=> 'empresas' do |company|
+  #     company.resources :users, :as => 'usuarios', 
+  #       :path_names => {:new => 'nuevo', :edit => 'editar', :create => 'crear'}, :shallow => true 
+  #   end
+  #   r.resources :surveys,    :as => 'encuestas'
+  #   r.resources :surveys,    :member => {:next_question => :get}
+  #   r.resources :answers,    :as => 'respuestas' 
+  #   r.resources :questions,  :as => 'preguntas'
+  #   r.resources :subsectors, :as => 'subsectores'
+  #   r.resources :chart,      :as => 'resultados'
+  # end
+  map.resources :questions
+  map.resources :answers
+  map.resources :companies ,:has_many => :users, :shallow => true
+  map.resources :subsectors
+  map.resources :surveys, :member => {:next_question => :get}
+  map.resources :chart
   map.resources :user_sessions
   map.resources :company_sessions
   map.resources :survey_records
