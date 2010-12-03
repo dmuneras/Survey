162390f5cf6f7b29c8b58ce9bf02f10f5d481001
donkike <karlysw@gmail.com>
2010-12-03 17:21:24 -0500

    added more chart support

--- app/models/survey_record.rb
+++ app/models/survey_record.rb
@@ -2,8 +2,9 @@ class SurveyRecord < ActiveRecord::Base
   belongs_to :user
   has_many :questions
 
-  def self.before_last_of(user)
-    SurveyRecord.find_all_by_user_id(user.id, :order => 'created_at DESC')[1]
+  def created_at
+    super.strftime("%d/%m/%Y")
   end
+
   
 end
