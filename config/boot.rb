a87f5b2db6ea64037d187125363157c9a8f15e9a
Daniel Múnera Sánchez <dmunera119@gmail.com>
2010-12-06 22:10:53 -0500

    add config bundle

--- config/boot.rb
+++ config/boot.rb
@@ -4,6 +4,20 @@
 RAILS_ROOT = "#{File.dirname(__FILE__)}/.." unless defined?(RAILS_ROOT)
 
 module Rails
+  class Rails::Boot
+    def run
+      load_initializer
+      
+      Rails::Initializer.class_eval do
+        def load_gems
+          @bundler_loaded ||= Bundler.require :default, Rails.env
+        end
+      end
+      
+      Rails::Initializer.run(:set_load_path)
+    end
+  end
+
   class << self
     def boot!
       unless booted?
